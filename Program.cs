d7ade0e2c35204a4a19686ee625e64371b1c9dcb
Lpanainte <Laura_ale14@yahoo.com>
2026-05-28 00:04:05 +0300

    create sqlite db

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
  options.UseSqlite(
         builder.Configuration.GetConnectionString("DefaultConnection")));
-
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated();
+}
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())

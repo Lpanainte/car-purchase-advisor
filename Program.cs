feba4e3259dc244d3aaa0354c7c64913067bc6a5
Lpanainte <Laura_ale14@yahoo.com>
2026-05-27 23:36:10 +0300

    sqlite fix

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
+ options.UseSqlite(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
